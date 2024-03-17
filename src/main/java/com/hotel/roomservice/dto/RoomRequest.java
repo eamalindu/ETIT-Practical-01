952c70b80fa65080defa8a9f72a97b94d2f6b506
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-17 11:43:06 +0530

    attributes added

--- src/main/java/com/hotel/roomservice/dto/RoomRequest.java
+++ src/main/java/com/hotel/roomservice/dto/RoomRequest.java
@@ -1,4 +1,16 @@
 package com.hotel.roomservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomRequest {
+
+    private String no;
+    private BigDecimal price;
 }
