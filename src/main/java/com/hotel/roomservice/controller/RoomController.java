8d46076fea32e51212bbb73f1ab78d85370a93f7
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-17 11:45:56 +0530

    Post mapping added

--- src/main/java/com/hotel/roomservice/controller/RoomController.java
+++ src/main/java/com/hotel/roomservice/controller/RoomController.java
@@ -1,9 +1,15 @@
 package com.hotel.roomservice.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
+
+    @PostMapping
+    public void create(){
+
+    }
 }
