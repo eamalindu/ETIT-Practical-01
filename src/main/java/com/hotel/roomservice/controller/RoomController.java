524913e00828348e1265b345c61065cd4f45199e
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-17 11:45:21 +0530

    url mapped

--- src/main/java/com/hotel/roomservice/controller/RoomController.java
+++ src/main/java/com/hotel/roomservice/controller/RoomController.java
@@ -1,4 +1,9 @@
 package com.hotel.roomservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/rooms")
 public class RoomController {
 }
