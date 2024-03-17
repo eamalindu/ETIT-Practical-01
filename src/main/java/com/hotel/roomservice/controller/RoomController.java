db952486e8dbaa95778c75e72f6babb820f76507
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-17 12:05:08 +0530

    ResponseStatus added

--- src/main/java/com/hotel/roomservice/controller/RoomController.java
+++ src/main/java/com/hotel/roomservice/controller/RoomController.java
@@ -1,16 +1,15 @@
 package com.hotel.roomservice.controller;
 
 import com.hotel.roomservice.dto.RoomRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/rooms")
 public class RoomController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody RoomRequest roomRequest){
         System.out.println("Room Service");
         System.out.println(roomRequest);
