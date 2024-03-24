7b41dca1c2827041ddb32f4c90bcfc61d812e236
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-24 11:16:05 +0530

    edited

--- src/main/java/com/hotel/roomservice/model/Room.java
+++ src/main/java/com/hotel/roomservice/model/Room.java
@@ -1,4 +1,23 @@
 package com.hotel.roomservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document(value = "room")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
+
+    @Id
+    private String id;
+
+    private String no;
+
+    private BigDecimal price;
 }
