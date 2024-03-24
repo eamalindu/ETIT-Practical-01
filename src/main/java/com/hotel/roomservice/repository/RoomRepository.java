e49e38b9d956d6caa0e11ab3c3a43e1db05e7036
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-24 11:30:04 +0530

    Dao initialize

--- src/main/java/com/hotel/roomservice/repository/RoomRepository.java
+++ src/main/java/com/hotel/roomservice/repository/RoomRepository.java
@@ -1,4 +1,7 @@
 package com.hotel.roomservice.repository;
 
-public interface RoomRepository {
+import com.hotel.roomservice.model.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room,String> {
 }
