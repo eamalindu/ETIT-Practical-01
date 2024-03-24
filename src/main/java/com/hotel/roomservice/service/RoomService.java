53ce7cbc0d682587ce5321830ee3ccc2f9028000
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-24 11:03:20 +0530

    create function created

--- src/main/java/com/hotel/roomservice/service/RoomService.java
+++ src/main/java/com/hotel/roomservice/service/RoomService.java
@@ -1,4 +1,12 @@
 package com.hotel.roomservice.service;
 
+import com.hotel.roomservice.dto.RoomRequest;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoomService {
+
+    public void create(RoomRequest){
+
+    }
 }
