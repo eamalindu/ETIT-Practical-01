d82b1dd6ee7ed64d8780cc81a05bc66f3c5c6d69
E A Malindu Prabodhitha <eamalindu@gmail.com>
2024-03-24 11:25:06 +0530

    edited

--- src/main/java/com/hotel/roomservice/service/RoomService.java
+++ src/main/java/com/hotel/roomservice/service/RoomService.java
@@ -1,6 +1,7 @@
 package com.hotel.roomservice.service;
 
 import com.hotel.roomservice.dto.RoomRequest;
+import com.hotel.roomservice.model.Room;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +10,11 @@ public class RoomService {
     public void create(RoomRequest roomRequest){
 
         System.out.println("Room Object from Service "+roomRequest);
+        Room rm = Room.builder()
+                .no(roomRequest.getNo())
+                .price(roomRequest.getPrice())
+                .build();
+
+        System.out.println(rm);
     }
 }
